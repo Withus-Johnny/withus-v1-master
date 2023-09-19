0fb4a034367726bad0a2f3d7612b977b97e71f3b
Withus-Johnny <withus.johnny@gmail.com>
2023-09-20 07:52:54 +0900

    기본 Publisher 프로젝트 업로드 - Client 배포 툴

---
+++ Publisher/Forms/PMain.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Publisher.Forms
+{
+    partial class PMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 187);
+            this.Name = "PMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PMain";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
