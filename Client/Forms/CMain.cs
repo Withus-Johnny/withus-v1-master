ee23d509ecd1274738150212f478e2522390b595
Withus-Johnny <withus.johnny@gmail.com>
2023-09-20 06:36:18 +0900

    Withus 기본 프로젝트 업로드 및 Client 추가 Client Runtime: `.NETFramework 4.7.2`

---
+++ Client/Forms/CMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Client.Forms
+{
+    public partial class CMain : Form
+    {
+        public CMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using Client.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace Client
+{
+    internal static class Program
+    {
+        public static CMain CMain;
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(CMain = new CMain());
+        }
+    }
+}
