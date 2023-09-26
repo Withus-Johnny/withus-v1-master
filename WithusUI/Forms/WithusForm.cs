03867062783f4e1926a77c2b3dfc9660ee5e8a72
withus.johnny <withus.johnny@gmail.com>
2023-09-27 02:39:24 +0900

    [WithusUI] 기본 프로젝트 업로드

---
+++ WithusUI/Forms/WithusForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WithusUI.Forms
+{
+    public partial class WithusForm : Form
+    {
+        public WithusForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
