c477efda60c4e600d67feef5e3aa4a812216ec5a
Withus-Johnny <withus.johnny@gmail.com>
2023-10-13 08:03:40 +0900

    `EmailValidator` 추가 - 허용 도메인 일부 추가

---
+++ Shared/Helpers/EmailValidator.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Shared.Helpers
+{
+    public static class EmailValidator
+    {
+        public static string[] _allowedDomains = { "naver.com", "daum.net",
+            "gmail.com", "yahoo.com", "hanmail.net", "nate.com", "msn.com" };
+
+        private static string GetDomainPattern()
+        {
+            return string.Join("|", _allowedDomains.Select(domain => Regex.Escape(domain)));
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            string domainPattern = GetDomainPattern();
+            string emailPattern = $@"^[a-zA-Z0-9._%+-]+@({domainPattern})$";
+
+            return Regex.IsMatch(email, emailPattern);
+        }
+    }
+}
